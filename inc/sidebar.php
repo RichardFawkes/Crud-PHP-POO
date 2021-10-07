87471ff4ee62d717d909eef9c5126b52f78eb63e
richard <richardhackers@gmail.com>
2021-10-07 02:08:42 -0300

    sidebar-icon-dinamico v1

--- inc/sidebar.php
+++ inc/sidebar.php
@@ -1,13 +1,23 @@
   <!-- Sidebar -->
+<?php $url = $_SERVER['REQUEST_URI']; ?>
+
   <nav id="sidebarMenu" class="collapse d-lg-block sidebar collapse bg-white">
       <div class="position-sticky">
         <div class="list-group list-group-flush mx-3 mt-4">
-        <a href="listar-todos.php" class="list-group-item list-group-item-action py-2 ripple">
-            <i class="fas fa-users fa-fw me-3"></i><span>Todos Devedores</span></a>
+        <a href="listar-todos.php" class="list-group-item list-group-item-action py-2 ripple  <?php 
+    if ($url == "/Teste-PHP-CRUD/listar-todos.php") {
+      echo "active";
+    }
+          ?>">
+            <i class="fas fa-users fa-fw me-3"></i><span>Listar Devedores</span></a>
 
 
 
-          <a href="cadastro.php" class="list-group-item list-group-item-action py-2 ripple active">
+          <a href="cadastro.php" class="list-group-item list-group-item-action py-2 ripple <?php 
+    if ($url == "/Teste-PHP-CRUD/cadastro.php") {
+      echo "active";
+    }
+          ?>">
             <i class="fas fa-chart-area fa-fw me-3"></i><span>Cadastrar Devedor</span>
           </a>
 
